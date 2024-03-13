3eae64db3b10c4d3532ec426743840ec1cd28759
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-03-13 20:29:08 +0300

    SeedCategoryTable

--- EBook/Data/ApplicationDbContext.cs
+++ EBook/Data/ApplicationDbContext.cs
@@ -11,5 +11,17 @@ namespace EBook.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        /*Pour implementer les donnees dans un tableau de la base de donnee sur Core on utullise la fonction
+        protected override void OnModelCreating(ModelBuilder modelBuilder){}*/
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
