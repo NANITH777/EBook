832a346b4312ef526d3181026c0d85f37279374f
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-03-13 20:57:36 +0300

    Category List

--- EBook/Controllers/CategoryController.cs
+++ EBook/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EBook.Data;
+using EBook.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EBook.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db=db;
+        }
         public IActionResult Index()
         {
+            //pour afficher la list des categories
+            List<Category> objetCategoryList= _db.Categories.ToList();
             return View();
         }
     }
