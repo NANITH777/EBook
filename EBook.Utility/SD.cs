21d0a029bef2ef41aaf8467cd3ac6b56f6fe9254
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-04-04 15:13:45 +0300

    Repository Interface

---
+++ EBook.Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBook.Utility
+{
+    public static class SD
+    {
+
+    }
+}
