83bac0cdd2fe57eb952fa737f9fecd4d1e21bd28
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-03 14:50:03 +0300

    Home Page

--- EBook/Areas/Customer/Controllers/HomeController.cs
+++ EBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using EBook.DataAccess.Repository;
+using EBook.DataAccess.Repository.IRepository;
 using EBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +10,18 @@ namespace EBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
