0f6f9f5730c4d3b87bef7c0fac9ecba700c9f124
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-03 19:17:21 +0300

    IdentityUser

---
+++ EBook.Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBook.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Required]
+        public int Name { get; set; }
+        public string? StressAdress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+
+
+    }
+}
