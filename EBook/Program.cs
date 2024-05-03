652e532892fab829c7a02614d36a182ff80b249b
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-03 17:07:23 +0300

    Identity

--- EBook/Program.cs
+++ EBook/Program.cs
@@ -2,6 +2,7 @@ using EBook.Data;
 using EBook.DataAccess.Repository;
 using EBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
