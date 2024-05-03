b1ec527e7a57350f56f6ee4b5055795030a266b8
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-03 17:15:03 +0300

    Update DbContext

--- EBook.DataAccess/Data/ApplicationDbContext.cs
+++ EBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using EBook.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EBook.Data
 {
-    public class ApplicationDbContext:IdentityDbContext
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
