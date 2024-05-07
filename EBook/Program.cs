0a69dbc77b479776faf6aff64b2183e02461eda5
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-07 19:47:16 +0300

    Update

--- EBook/Program.cs
+++ EBook/Program.cs
@@ -13,8 +13,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>
-    (options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
