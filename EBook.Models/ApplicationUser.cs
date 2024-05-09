f9a809803d7546c5a4603a5bed53eab6c3d3d16d
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-09 10:18:27 +0300

    Company Update

--- EBook.Models/ApplicationUser.cs
+++ EBook.Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,10 @@ namespace EBook.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? ComapnyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
 
 
     }
