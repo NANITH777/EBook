daf595883a062637b53299e44268bd501fa0acfe
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-09 21:25:38 +0300

    Update Shopping Cart

--- EBook/Areas/Customer/Controllers/CartController.cs
+++ EBook/Areas/Customer/Controllers/CartController.cs
@@ -15,11 +15,9 @@ namespace EBook.Areas.Customer.Controllers
         private readonly IUnitOfWork _unitOfWork;
         [BindProperty]
         public ShoppingCartVM shoppingCartVM { get; set; }
-        //private readonly IEmailSender _emailSender;
-        public CartController(IUnitOfWork unitOfWork /*IEmailSender emailSender*/)
+        public CartController(IUnitOfWork unitOfWork )
         {
             _unitOfWork = unitOfWork;
-            //_emailSender = emailSender;
         }
 
         public IActionResult Index()
@@ -63,9 +61,7 @@ namespace EBook.Areas.Customer.Controllers
             if (cartFromDb.Count <= 1)
             {
                 //remove that from cart
-                //HttpContext.Session.SetInt32(Commun.SessionCart,
-                //_unitOfWork.ShoppingCart
-                //.GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+               
                 _unitOfWork.ShoppingCart.Remove(cartFromDb);
             }
             else
@@ -82,8 +78,7 @@ namespace EBook.Areas.Customer.Controllers
         public IActionResult Remove(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
-            //HttpContext.Session.SetInt32(Commun.SessionCart,
-            //   _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+            
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
             _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
