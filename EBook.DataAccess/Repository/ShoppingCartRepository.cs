692b06a842bd96a1d788e17b582d54b9f1df585a
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-09 14:15:20 +0300

    ShoppingCart Repository

---
+++ EBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,27 @@
+﻿using EBook.Data;
+using EBook.DataAccess.Repository.IRepository;
+using EBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private  ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+
+    }
+}
--- EBook.DataAccess/Repository/UnitOfWork.cs
+++ EBook.DataAccess/Repository/UnitOfWork.cs
@@ -15,12 +15,14 @@ namespace EBook.DataAccess.Repository
         public ICategoryRepository Category{ get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
         }
 
         
