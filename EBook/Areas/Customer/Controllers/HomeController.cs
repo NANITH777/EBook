8f6c8296746a431ce3199671111e2b141d9a9dcd
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-09 18:57:57 +0300

    Add Cart Update

--- EBook/Areas/Customer/Controllers/HomeController.cs
+++ EBook/Areas/Customer/Controllers/HomeController.cs
@@ -47,9 +47,24 @@ namespace EBook.Areas.Customer.Controllers
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
             shoppingCart.ApplicationUserId = userId;
 
-            _unitOfWork.ShoppingCart.Add(shoppingCart);
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(
+               u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+            if (cartFromDb != null)
+            {
+                //shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                //add cart record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+               
+            }
+
             _unitOfWork.Save();
-            
+
             return RedirectToAction(nameof(Index));
         }
 
