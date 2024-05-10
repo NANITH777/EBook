c6b3ec0c505715a0e554b04f58ba732b95077245
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-10 17:14:05 +0300

    Helper Method in Order Header

--- EBook.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ EBook.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -11,5 +11,7 @@ namespace EBook.DataAccess.Repository.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader obj);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
     }
 }
--- EBook.Models/OrderHeader.cs
+++ EBook.Models/OrderHeader.cs
@@ -28,6 +28,7 @@ namespace EBook.Models
         public DateTime PaymentDate { get; set; }
         public DateTime PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
