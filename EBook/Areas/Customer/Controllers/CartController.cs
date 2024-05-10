8e76821c7d32e99d8495eb1a86a4a2c4bb3910a0
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-10 18:55:55 +0300

    Update

--- EBook/Areas/Customer/Controllers/CartController.cs
+++ EBook/Areas/Customer/Controllers/CartController.cs
@@ -132,8 +132,7 @@ namespace EBook.Areas.Customer.Controllers
                 //it is a regular customer account and we need to capture payment
                 // stripe logic
 
-                var domain = "https://localhost:7269/";
-
+                var domain = "https://localhost:7280/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={shoppingCartVM.OrderHeader.Id}",
@@ -181,6 +180,27 @@ namespace EBook.Areas.Customer.Controllers
 
         public IActionResult OrderConfirmation(int id)
         {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == id, includeProperties: "ApplicationUser");
+            if (orderHeader.PaymentStatus != SD.PaymentStatusDelayedPayment)
+            {
+                //this is an order by customer
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeader
+                    .UpdateStripePaymentID(id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+
+            }
+
+            List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll
+                (u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+            _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+            _unitOfWork.Save();
             return View(id);
         }
 
