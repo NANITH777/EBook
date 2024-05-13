ece494c2390c3a6d158983501986cc03eded4a4b
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-13 21:47:06 +0300

    View Component

--- EBook/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ EBook/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -27,7 +27,6 @@ namespace EBook.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            HttpContext.Session.Clear();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
---
+++ EBook/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using EBook.DataAccess.Repository;
+using EBook.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EBook.ViewComponents
+{
+    public class ShoppingCartViewComponent: ViewComponent
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                if(HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll
+                   (u => u.ApplicationUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
