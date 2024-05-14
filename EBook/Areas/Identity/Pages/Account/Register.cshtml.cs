cf767b160ad6d49a89676549bcb5c7abe5a0ee76
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-14 16:12:29 +0300

    Create users from by admin panel

--- EBook/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EBook/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -212,7 +212,15 @@ namespace EBook.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if(User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
+                        
                         return LocalRedirect(returnUrl);
                     }
                 }
