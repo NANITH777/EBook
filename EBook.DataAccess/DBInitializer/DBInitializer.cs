6237e1e89c7a28c8961975d0f2d9d678e04287dc
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-15 11:33:35 +0300

    DBInitializer

---
+++ EBook.DataAccess/DBInitializer/DBInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBook.DataAccess.DBInitializer
+{
+    public class DBInitializer : IDBInitializer
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
