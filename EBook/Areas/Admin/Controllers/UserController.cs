7d0ef87e60797f66025fc3c592bf49c53027ee8c
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-16 18:02:27 +0300

    Lock Inlock Method

--- EBook/Areas/Admin/Controllers/UserController.cs
+++ EBook/Areas/Admin/Controllers/UserController.cs
@@ -48,10 +48,27 @@ namespace EBook.Areas.Admin.Controllers
             return Json(new  { data = objUserList });
         }
 
-        [HttpDelete]
-        public IActionResult Delete(int? id)
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
         {
-            return Json(new { success = true, message = "Delete successful" });
+
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Delete Successful" });
         }
         #endregion
     }
