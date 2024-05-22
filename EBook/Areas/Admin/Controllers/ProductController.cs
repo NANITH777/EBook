c4d5a3596d42a33be6d71de52af20e629d96003b
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-22 10:47:41 +0300

    Update Delete Product

--- EBook/Areas/Admin/Controllers/ProductController.cs
+++ EBook/Areas/Admin/Controllers/ProductController.cs
@@ -170,13 +170,18 @@ namespace EBook.Areas.Admin.Controllers
                 return Json(new {success = false, messsage = "Error while deleting"});
             }
 
-            //var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, 
-            //    productToBeDeleted.ImageUrl.TrimStart('\\'));
+            string productPath = @"images\products\product-" +id;
+            string finalPath = Path.Combine(_webHostEnvironment.WebRootPath, productPath);
 
-            //if (System.IO.File.Exists(oldImagePath))
-            //{
-            //    System.IO.File.Delete(oldImagePath);
-            //}
+            if (Directory.Exists(finalPath))
+            {
+                string[] filePaths = Directory.GetFiles(finalPath);
+                foreach (string filePath in filePaths)
+                {
+                    System.IO.File.Delete(filePath);
+                }
+                Directory.Delete(finalPath);
+            }
 
             _unitOfWork.Product.Remove(productToBeDeleted);
             _unitOfWork.Save();
