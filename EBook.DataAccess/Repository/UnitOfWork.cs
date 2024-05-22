0e13903c2e4b4cc44c79fdf8d5be7fba8f42e67a
NANITH777 <109669139+NANITH777@users.noreply.github.com>
2024-05-22 09:29:17 +0300

    ProductImage Repository

--- EBook.DataAccess/Repository/UnitOfWork.cs
+++ EBook.DataAccess/Repository/UnitOfWork.cs
@@ -19,6 +19,7 @@ namespace EBook.DataAccess.Repository
         public IApplicationUserRepository ApplicationUser { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
+        public IProductImageRepository ProductImage { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
@@ -29,6 +30,7 @@ namespace EBook.DataAccess.Repository
             ApplicationUser = new ApplicationUserRepository(_db);
             OrderHeader = new OrderHeaderRepository(_db);
             OrderDetail = new OrderDetailRepository(_db);
+            ProductImage = new ProductImageRepository(_db);
         }
 
         
